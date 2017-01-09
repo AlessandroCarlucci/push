f5356602906ba6d711ac4adc5a0ba70aeb0dfe50
AlessandroCarlucci <ing.alessandro.carlucci@gmail.com>
2017-01-09 16:50:31 +0100

    f

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,12 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
-        alert("ok");
+        window.FirebasePlugin.getToken(function(token) {
+    // save this server-side and use it to push notifications to this device
+    alert(token);
+}, function(error) {
+   alert(error);
+});
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
