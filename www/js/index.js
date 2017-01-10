e9ed5bf6be2444ee4dd52951ace9dcc98c64ecfd
AlessandroCarlucci <ing.alessandro.carlucci@gmail.com>
2017-01-10 15:19:05 +0100

    prova

--- www/js/index.js
+++ www/js/index.js
@@ -34,6 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        window.FirebasePlugin.setBadgeNumber(3);
  window.FirebasePlugin.getToken(function(token) {
     // save this server-side and use it to push notifications to this device
     alert(token);
